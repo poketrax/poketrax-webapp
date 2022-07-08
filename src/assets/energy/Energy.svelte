34968c2b199f3fbe710e16192adaa3ceb493acdc
jgunzelman <jgunzelman88@gmail.com>
2022-07-08 16:05:14 -0400

    💄 add initial assets

---
+++ src/assets/energy/Energy.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Pokeball from "./pokeball.svelte";
+    export let type = "";
+    export let className = "";
+</script>
+
+{#if type === ""}
+    <Pokeball className={className}/>
+{:else}
+    <img class={className} alt={type} src={`./assets/energy/${type}.png`} />
+{/if}
