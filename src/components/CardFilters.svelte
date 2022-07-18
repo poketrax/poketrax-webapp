31c0ae25169c505babc2884e870c4c28292bb894
jgunzelman <jgunzelman88@gmail.com>
2022-07-18 14:44:37 -0400

    👔  add more logic to support set filtering

--- src/components/CardFilters.svelte
+++ src/components/CardFilters.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
+    import { setStore } from "../lib/Stores";
+    import Svelecte from "svelecte/src/Svelecte.svelte";
     import Icon from "@smui/textfield/icon";
     import type { Writable } from "svelte/store";
     import type { SearchTerms } from "../lib/Stores";
     import Textfield from "@smui/textfield";
+    import type { Expansion } from "src/lib/Meta";
 
     export let searchStore: Writable<SearchTerms>;
 
     let keyword = "";
+    let labelAsValue = false;
+    let selectedSets: Array<Expansion> = [];
+    let setOptions: Array<Expansion> = [];
+    let options: string[] = setOptions.map((opt) => opt.name);
+
+    setStore.subscribe((data) => {
+        setOptions = data;
+        console.log(JSON.stringify(setOptions));
+        options = setOptions.map((opt) => opt.name);
+    });
 
     function keywordSearch() {
         searchStore.update((val) => {
@@ -24,3 +37,7 @@
         label="Keyword"
     />
 </div>
+<div class="p-4">
+
+</div>
+
