dcc6fa47aff7245845d3b292b1cd80b5ca30ce3b
jgunzelman <jgunzelman88@gmail.com>
2022-07-18 09:14:09 -0400

    💄 Added car filters

---
+++ src/components/CardFilters.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Icon from "@smui/textfield/icon";
+    import type { Writable } from "svelte/store";
+    import type { SearchTerms } from "../lib/Stores";
+    import Textfield from "@smui/textfield";
+
+    export let searchStore: Writable<SearchTerms>;
+
+    let keyword = "";
+
+    function keywordSearch() {
+        searchStore.update((val) => {
+            val.keyword = keyword;
+            return val;
+        });
+    }
+</script>
+
+<div class="p-4">
+    <Textfield
+        variant="outlined"
+        bind:value={keyword}
+        on:change={keywordSearch}
+        label="Keyword"
+    />
+</div>
